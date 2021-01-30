579fc16c50d090d9c75f926695401905eb8c2b58
AlexanderAgopov <alexander.agopov@gmail.com>
2021-01-30 22:21:53 +0200

    add redux persistence

--- witch-grimoire/src/index.js
+++ witch-grimoire/src/index.js
@@ -5,25 +5,23 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 import configureStore from "./state/configureStore.js";
-import { Provider} from "react-redux";
+import { Provider } from "react-redux";
+import { PersistGate } from 'redux-persist/integration/react'
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-
-const store = configureStore();
+const storeObj = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-
+    <PersistGate loading={null} persistor={storeObj.persistor}>
+      <Provider store={storeObj.store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </PersistGate>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
--- witch-grimoire/src/state/configureStore.js
+++ witch-grimoire/src/state/configureStore.js
@@ -2,8 +2,17 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 export default function configureStore(initialState) {
+    const persistConfig = {
+        key: 'root',
+        storage,
+    }
+    const persistedReducer = persistReducer(persistConfig, rootReducer)
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    return createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())));
+    let store = createStore(persistedReducer, initialState, composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())));
+    let persistor = persistStore(store)
+    return { store, persistor }
 }
